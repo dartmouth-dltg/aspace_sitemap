a15d064a04a31c90af07cc08b713559c1ce86ec3
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2024-09-25 17:42:04 -0400

    first pass at AS 4.0

--- frontend/helpers/aspace_form_helper.rb
+++ frontend/helpers/aspace_form_helper.rb
@@ -1,21 +1,45 @@
 module AspaceFormHelper
   class FormContext
     
+    # pilfered from OAI checkboxes
     def label_and_multi_boolean(name, items, opts = {}, default = false, force_checked = false)
-      opts[:col_size] = 1
-      opts[:controls_class] = "checkbox"
-      multi_checkbox_html = ""
-      items.each do |item|
-        multi_checkbox_html << label_with_field(name+"_"+item, multi_checkbox(name, item, opts, default, force_checked), opts)
+
+      html = ""
+      html << "<div class='form-group'>"
+      html << label("sitemap_types", {}, ["control-label", "col-sm-3"])
+      html << "<div class='col-sm-9'>"
+      html << "<ul class='checkbox-list'>"
+      items.each do |v|
+        # if we have an empty list of checkboxes, assume all sets are enabled.
+        # otherwise, a checkbox is on if it's the in the list we get from the backend.
+
+        html << "<li class='list-group-item'>"
+        html << "<div class='checkbox'>"
+        html << "<label>"
+        html << "<input id=\"#{id_for(name)}_#{v}\" name=\"#{path(name)}[]\" value=\"#{v}\" type=\"checkbox\" "
+
+        if force_checked or (obj[name] === true) or (obj[name].is_a? String and obj[name].start_with?("true")) or (obj[name] === "1") or (obj[name].nil? and default)
+          html << "checked=\"checked\" "
+        end
+
+        if readonly?
+          html << "disabled />"
+        else
+          html << "/>"
+        end # of checkbox tag
+
+        html << "#{I18n.t("aspace_sitemap_job.sitemap_types_#{v}")}"
+        html << "</label>"
+        html << "</div>"
+        html << "</li>"
       end
-      multi_checkbox_html.html_safe   
+      html << "</ul>"
+      html << "</div>" #col-sm-9
+      html << "</div>" #form-group
+
+      html.html_safe
+     
     end
-    
-    def multi_checkbox(name, item, opts = {}, default = true, force_checked = false)
-      options = {:id => "#{id_for(name)}_#{item}", :type => "checkbox", :name => path(name)+"[]", :value => "#{item}"}
-      options[:checked] = "checked" if force_checked or (obj[name] === true) or (obj[name].is_a? String and obj[name].start_with?("true")) or (obj[name] === "1") or (obj[name].nil? and default)
-  
-      @forms.tag("input", options.merge(opts), false, false)
-    end
+
   end
 end
